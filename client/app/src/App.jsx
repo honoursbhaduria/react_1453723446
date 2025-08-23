a0ea8807d2d5343c0f5cc16da3a1011b354536e6
HonoursBhaduria <honoursbhadauria@gmail.com>
2025-08-23 23:04:10 +0530

    dajngo file structured

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,26 +7,16 @@ function App() {
 
   return (
     <>
+      <h1>
+        Books website 
+      </h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" placeholder='Book title....' />
+        <br />
+        <input type="number" placeholder='Release Date....' />
+        <br />
+        <button>Add Book</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
