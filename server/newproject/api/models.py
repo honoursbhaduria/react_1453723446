694c955fb40c4212a0902566466a846cf0580819
HonoursBhaduria <honoursbhadauria@gmail.com>
2025-08-23 23:15:13 +0530

    created models for books

--- server/newproject/api/models.py
+++ server/newproject/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    release_year = models.IntegerField()
+
+def __str__(self):
+    return self.title
